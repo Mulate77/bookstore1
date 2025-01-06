a1f3bfdddd0d5f28704741a0470b11740a3d7173
Mulate77 <myratatayew77@icloud.com>
2025-01-06 20:20:18 +0500

    1.0.2

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -9,5 +9,20 @@ class Lesson extends Model
 {
 
     use HasFactory;
-
+public function student()
+{
+    return $this->belongsToMany(Student::class);
+}
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
 }
